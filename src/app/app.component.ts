bd5e8f65014c118d865052bba2a7c1888ce9fb55
gabriel <gabrielgac75@gmail.com>
2017-11-29 18:21:06 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Piñas Cake';
+  a = 4;
+  b = 5;
+  listo = false;
+
+  contructor(){
+  	setTimeout(() => {
+  		this.listo = true;
+  	}, 3000)
+  }
 }
