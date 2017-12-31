7aa2bac5e0958d05c8fa52a366526593801416f6
Gabriel <gabrielgac75@gmail.com>
2017-12-31 14:30:36 -0400

    ruta de contacto y active de navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
-
-
 import { AppComponent } from './app.component';
+import { Routes, RouterModule} from "@angular/router";
+import { ContactoComponent } from './contacto/contacto.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
+const appRoutes: Routes = [
+	{path: '', component: DetalleComponent},
+  {path: 'index', component: DetalleComponent},
+	{path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
